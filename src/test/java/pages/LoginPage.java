d0122ee6826250c709f561fabf1d442cd55a6d93
~!@#$%^&*()_+}{ <88084445+emrahselek@users.noreply.github.com>
2021-11-18 17:44:09 -0700

    "es"

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class LoginPage {
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+
+
+
+
+
+}
---
+++ src/test/java/pages/RegisterPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class RegisterPage {
+    public RegisterPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+
+
+
+
+
+}
